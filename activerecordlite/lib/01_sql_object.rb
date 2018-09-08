0c29fac1fe0af98dd59ff7ea69a8c85da0c5b8de
Chris Yip <chris.yip87@gmail.com>
2018-09-07 17:00:41 -0700

    Finish up to #insert

---
+++ activerecordlite/lib/01_sql_object.rb
@@ -0,0 +1,121 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    # ...
+    return @columns if @columns
+    arr = []
+    str = 'select * FROM ' << self.table_name
+    DBConnection.execute2(str)[0].each {|el| arr << el.to_sym}
+    @columns = arr
+
+  end
+
+  def self.finalize!
+
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |value|
+        self.attributes[col]=value
+      end
+
+    end
+  end
+
+  def self.table_name=(table_name)
+    # ...
+    @table_name=table_name
+  end
+
+  def self.table_name
+    # ...
+    @table_name ||= self.name.tableize
+  end
+
+  def self.all
+    # ...
+    parse_all(DBConnection.execute(<<-SQL)
+      SELECT
+      #{table_name}.*
+      FROM
+      #{table_name}
+    SQL
+    )
+
+  end
+
+  def self.parse_all(results)
+    # ...
+    results.map {|el| self.new(el)}
+  end
+
+  def self.find(id)
+    # ...
+    results = DBConnection.execute(<<-SQL,id)
+    SELECT
+    *
+    FROM
+    #{table_name}
+    WHERE
+    id = ?
+    SQL
+    return nil if results.empty?
+    self.new(results.first)
+  end
+
+  def initialize(params = {})
+    # ...
+    params.each do |k,v|
+      key=k.to_sym
+      raise "unknown attribute '#{key}'" unless self.class.columns.include?(key)
+
+    send("#{key}=",v)
+    end
+
+  end
+
+  def attributes
+    # ...
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+    self.class.columns.map{|attr| self.send(attr) }
+  end
+  #
+  # q = "( "
+  # [29] pry(main)*   x.times do |el|
+  # [29] pry(main)*     q << "?"
+  # [29] pry(main)*     q << " " unless el == x-1
+  # [29] pry(main)*   end
+  # [29] pry(main)*   q << ")"
+
+  def insert
+    # ...
+    col_names = self.class.columns.count
+    question_marks = "("
+    col_names.times do |x|
+      question_marks << "?"
+      question_marks << " " unless x == col_names-1
+    end
+    quesiton_marks << ")"
+
+    DBConnection.last_insert_row_id
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
